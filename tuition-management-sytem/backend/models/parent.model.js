0795e5e5873385296c461a4ba4dc3de4df995917
Vinnath Pathirana <125301413+VinnathPathirana@users.noreply.github.com>
2023-04-07 05:53:03 +0530

    add parent function
    correct the add parent function errors

--- tuition-management-sytem/backend/models/parent.model.js
+++ tuition-management-sytem/backend/models/parent.model.js
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 
 const ParentSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    required: true,
-    unique : true,
-  },
   name: {
     type: String,
     required: true,
