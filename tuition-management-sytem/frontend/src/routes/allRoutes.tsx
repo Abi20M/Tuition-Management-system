48c573e5737455911c139c1b7e2d0901a18ee280
Vinnath Pathirana <125301413+VinnathPathirana@users.noreply.github.com>
2023-04-09 15:26:36 +0530

    create parent login page

--- tuition-management-sytem/frontend/src/routes/allRoutes.tsx
+++ tuition-management-sytem/frontend/src/routes/allRoutes.tsx
@@ -7,10 +7,12 @@ import { AllLogin } from "../pages/AllLogin/AllLogin";
 import Page404 from "../pages/Page404";
 import AdminPrivateRoute from "./adminPrivateRoute";
 import ExpensePrivateRoute from "./expensePrivateRoute";
+import ParentPrivateRoute from "./ParentPrivateRoute";
 
 
 import {ExpenseDashboard} from "../pages/expenseDashboard/index";
 import { StudentLoginPage } from "../pages/AllLogin/student-login";
+import { ParentLogin } from "../pages/AllLogin/Parent";
 
 export const AllRoutes = () => {
   return (
@@ -20,6 +22,7 @@ export const AllRoutes = () => {
         <Route path="admin/login" element={<AdminLoginPage />} />
         <Route path = "financial/login" element={<FinancialManagerLoginPage />} />
         <Route path="/student/login" element={<StudentLoginPage />} />
+        <Route path = "parent/login" element={<ParentLogin/>} />
         <Route path="/logout" element={<Logout />} />
       
         <Route path="*" element={<Page404/>}/>
@@ -32,6 +35,11 @@ export const AllRoutes = () => {
         <Route path = "/financial/" element = {<ExpensePrivateRoute/>}>
           <Route path ="/financial/dashboard" element={<ExpenseDashboard />} />
         </Route>
+
+        {/* <Route path = "/parent/" element = {<ParentPrivateRoute/>}>
+          <Route path = "/parent/login" element={<ParentLogin/>} />
+
+        </Route>   */}
 
               
       </Routes>
