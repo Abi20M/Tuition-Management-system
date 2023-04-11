847d33bbac52eaa3d12d9ec70c09f2e29e2a00ff
IT21232954 <99704372+IT21232954@users.noreply.github.com>
2023-04-12 02:51:38 +0530

    update admin  management
    update admin  management

--- tuition-management-sytem/frontend/src/components/ManageAdmin/index.tsx
+++ tuition-management-sytem/frontend/src/components/ManageAdmin/index.tsx
@@ -376,7 +376,7 @@ const ManageAdmins: React.FC = () => {
       telephone: (value) =>
           value.length < 11 ? "Telephone must have at least 10 numbers" : null,
 
-    //
+    
     },
   });
 
