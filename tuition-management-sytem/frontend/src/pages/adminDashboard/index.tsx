040c15ebb3294978279c17cb9bdb2c2a1509cf7b
Sahan Bandara <92173358+sahanbandara23623@users.noreply.github.com>
2023-04-24 13:04:11 +0530

    Student Report Generation Part

--- tuition-management-sytem/frontend/src/pages/adminDashboard/index.tsx
+++ tuition-management-sytem/frontend/src/pages/adminDashboard/index.tsx
@@ -106,7 +106,7 @@ export const AdminDashboard = () => {
         <ManageTeachers/>
         </Tabs.Panel>
         <Tabs.Panel value="Students">
-          <ManageStudents/>
+          <ManageStudents user={user}/>
         </Tabs.Panel>
         <Tabs.Panel value="Parents">
           <ManageParents/>
