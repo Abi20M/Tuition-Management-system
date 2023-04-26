b9528b4c0932afeb426c6fc0b643729594f01c44
Chathum10 <jsdilshan40@gmail.com>
2023-04-26 14:53:35 +0530

    Create fee.model.js

---
+++ tuition-management-sytem/backend/models/fee.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const FeeSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required : true,
+    unique : true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: 'Not Paid'
+  },  
+},{timestamps:true});
+
+const Fee = mongoose.model("Fees", FeeSchema);
+
+export default Fee;
